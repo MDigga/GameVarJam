1dbdae44efff2067f3773fc20dc6b9509d450d18
sourtangerine <bimoktsr229@gmail.com>
2026-09-15 21:07:20 +0700

    polished movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+var last_direction: Vector2 = Vector2.RIGHT
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -10,8 +12,28 @@ func _physics_process(delta: float) -> void:
 
 
 func process_movement() -> void:
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction := Input.get_vector("left", "right", "up", "down")
+	
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = Vector2.ZERO
+	
+	process_animation(last_direction)
+
+func process_animation(direction) -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("run", direction)
+	else:
+		play_animation("idle", direction)
+	
+
+func play_animation(prefix: String, dir: Vector2) -> void:
+	if dir.x != 0:
+		animated_sprite_2d.flip_h = dir.x < 0
+		animated_sprite_2d.play(prefix + "_right")
+	elif dir.y < 0:
+		animated_sprite_2d.play(prefix + "_up")
+	elif dir.y > 0:
+		animated_sprite_2d.play(prefix + "_down")
